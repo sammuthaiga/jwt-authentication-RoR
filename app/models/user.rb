b1a5612b827a3234c4fb7f9cde82a838d6fb1807
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-05-26 02:04:03 +0300

    feat: complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
     has_secure_password
-  mount_uploader :avatar, AvatarUploader
+#   mount_uploader :avatar, AvatarUploader
   validates :email, presence: true, uniqueness: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :username, presence: true, uniqueness: true
   validates :password,
-            length: { minimum: 6 },
+            length: { minimum: 5 },
             if: -> { new_record? || !password.nil? }
 end
